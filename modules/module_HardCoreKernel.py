e52cb47e4ffbd1fd5bf3ad675e4c1878ce49975d
Bruno Rousseau <rousseau.bruno@gmail.com>
2014-11-13 17:16:59 -0500

    Removed gamma2, turned gamma1 to gamma.
    There is now a single kernel function.

--- modules/module_HardCoreKernel.py
+++ modules/module_HardCoreKernel.py
@@ -17,28 +17,16 @@ k_cutoff = N.sqrt(2.*N.pi/Area) # in 1/bohr
 D_cutoff = hvF*k_cutoff         # in eV
 
 
-gamma1_constant =  -2.*N.pi*hvF**2/D_cutoff
-gamma2_constant =  N.sqrt(3.)*acell*D_cutoff/(12.*hvF)
+gamma_constant =  -2.*N.pi*hvF**2/D_cutoff
 
 
-def get_gamma1(list_epsilon):
+def get_gamma(list_epsilon):
     """
     The argument should be epsilon_{nk} = xi_{nk}+mu, in eV.
 
     The returned value is in eV x a0^2
     """
-    gamma1 = complex(1.,0.)*gamma1_constant*list_epsilon/D_cutoff
+    gamma = complex(1.,0.)*gamma_constant*list_epsilon/D_cutoff
 
-    return gamma1
-
-def get_gamma2(list_epsilon):
-    """
-    The argument should be epsilon_{nk} = xi_{nk}+mu, in eV
-
-    The returned value is in eV x a0^2
-    """
-
-    gamma2 = complex(1.,0.)*gamma1_constant*gamma2_constant*N.ones_like(list_epsilon)
-
-    return gamma2
+    return gamma
 
