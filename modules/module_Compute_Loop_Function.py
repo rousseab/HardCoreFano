0012728a0a76daf65303a401ed56f9e9ad3b6389
Bruno Rousseau <rousseau.bruno@gmail.com>
2014-11-25 18:43:22 -0500

    Modified loop computation to remove matsubara sum.

--- modules/module_Compute_Loop_Function.py
+++ modules/module_Compute_Loop_Function.py
@@ -17,6 +17,93 @@ from module_Integrators import *
 
 class Compute_Loop_Function:
 
+    def __init__(self, mu, beta, q_vector, E_phonon_polarization, hw_ph, grid, external_list_hw, Gamma_width):
+
+        self.mu      = mu
+        self.beta    = beta
+
+        self.q       = deepcopy(q_vector)
+        self.E_ph    = deepcopy(E_phonon_polarization)
+        self.hw_ph   = deepcopy(hw_ph)
+        self.grid    = deepcopy(grid)
+
+        self.list_hw = external_list_hw 
+        self.nhw     = len(external_list_hw)
+
+        # Widths
+        self.Gamma   = Gamma_width
+
+
+        # Normalization contains change of measure in going from sum to integral
+        # on k, also sum on spin:
+        #
+        #   1/Omega \sum_{k sigma} (...) -->  2/Omega x Omega/(2 pi)^2 int d2k (...)
+        #                                    --> 1/2 pi^2 int d2k (...)
+
+        self.normalization = 1./(2.*N.pi**2)
+
+        # columns loop faster in C (and thus in python)
+        # First index is for x or y, second index is for frequency
+
+        self.initialize_Hq()
+
+    def initialize_Hq(self):
+        """
+        Hq will be in fundamental units of [charge] x [velocity], e hbar / m a_0
+        """
+        # Hq will be in fundamental units of [charge] x [velocity], e hbar / m a_0
+
+        # first index   : alpha = x, y
+        # second index  : L = 'A','B' the impurity scattering site index
+        # third index   : elph coupling parameters u, v
+        self.Hq = complex(0.,0.)*N.zeros([2, 2, 2, self.nhw])
+        return
+
+
+    def Compute_Hq(self):
+
+        # Loop on wedges in the 1BZ
+        for wedge in self.grid.list_wedges:
+
+            # Compute matrix elements
+            Mq = MGridFunction(q_vector=self.q,E_phonon_polarization=self.E_ph,hw_nu_q=self.hw_ph,wedge=wedge)
+
+            # Compute the I function, which contains frequency dependence
+            Iq = IGridFunction(q_vector=self.q,list_hw=self.list_hw, delta_width=self.Gamma, mu=self.mu, beta=self.beta,  wedge=wedge)
+
+            for n1 in [0,1]:
+                for n2 in [0,1]:
+                    for n3 in [0,1]:
+
+                        I_key = (n1,n2,n3)
+                        I_index = Iq.index_dictionary[I_key]
+                        # dimensions [nk,nw]
+                        IElements = Iq.I[I_index,:,:]
+
+                        for i_alpha, alpha in zip([0,1],['x','y']):
+                            for i_L, L in zip([0,1],['A','B']):
+
+
+                                M_key   = (alpha,L,n1,n2,n3)
+                                M_index = Mq.index_dictionary[M_key]
+
+
+                                # dimension nk
+                                MatrixElements_u = Mq.M[M_index,0,:]
+                                MatrixElements_v = Mq.M[M_index,1,:]
+
+                                MI_u = MatrixElements_u[:,N.newaxis]*IElements 
+                                MI_v = MatrixElements_v[:,N.newaxis]*IElements 
+
+                                self.Hq[i_alpha,i_L, 0,:] += self.normalization*AreaIntegrator(wedge,MI_u)
+                                self.Hq[i_alpha,i_L, 1,:] += self.normalization*AreaIntegrator(wedge,MI_v)
+
+        return
+
+
+
+class Compute_Loop_Function_MATSUBARA_SUMS:
+
     def __init__(self, mu, beta, q_vector, E_phonon_polarization, hw_ph, grid, external_list_hw, Gamma_width, matsubara_cutoff_energy):
 
         self.mu      = mu
@@ -49,7 +136,6 @@ class Compute_Loop_Function:
         # First index is for x or y, second index is for frequency
 
         self.initialize_Hq()
-
 
     def initialize_Hq(self):
         """
