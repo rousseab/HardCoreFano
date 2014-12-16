b498f22b4b10371588de963337a687af4346b5d7
Bruno Rousseau <rousseau.bruno@gmail.com>
2014-12-16 14:33:20 -0500

    Modified computation of kernel functions to account for finite kernel_Gamma_width.

--- modules/module_HardCoreKernel.py
+++ modules/module_HardCoreKernel.py
@@ -22,126 +22,6 @@ from scipy.interpolate import splmake, spleval, spline
 k_cutoff = N.sqrt(2.*N.pi/Area) # in 1/bohr
 D_cutoff = hvF*k_cutoff         # in eV
 
-gamma_constant =  -2.*N.pi*hvF**2/D_cutoff
-
-
-def get_gammaA(list_z):
-    """
-    THIS CODE IS OBSOLETE.
-
-    The "real" kernel function is given by
-
-        gamma(z) =  2 pi (hvF)^2/D ( D/z 1/ln[1-D^2/z^2])
-
-    This function is wildly non-analytic near zero. We thus wish to
-    extract its behavior at inifnity.    
-
-    z-> infinity limit:
-    gamma_inf (z) =- 2 pi (hvF)^2/D  z/D
-
-    Pade approximation of the leftover, to first order:
-    gamma_1   (z) =  2 pi (hvF)^2/D  ( 3x^3+2x)/(6x^4+3x^2+2),  x  = z/D
-
-    gammaA = gamma_inf + gamma_1 
-
-           = 2 pi (hvF)^2/D [-  x^5/(x^4 + 1/2 x^2 + 1/3)]
-
-    The argument of the function should always be offset by mu!
-
-    The returned value is in eV x a0^2
-    """
-
-    prefactor =  complex(1.,0.)*2.*N.pi*hvF**2/D_cutoff
-
-    x = list_z/D_cutoff
-    x2= x*x
-    x3= x2*x
-    x4= x3*x
-    x5= x4*x
-
-    numerator   = -x5
-    denominator = complex(1.,0.)*x4+complex(0.5,0.)*x2+complex(1.,0.)/complex(3.,0)
-
-    gammaA   = prefactor*numerator/denominator 
-
-    return gammaA
-
-def get_gammaA_POLES_and_RESIDUES():
-    """
-    THIS CODE IS OBSOLETE.
-
-    gammaA = 2 pi (hvF)^2/D [-  x^5/(x^4 + 1/2 x^2 + 1/3)]
-
-    The poles are given by  Z_j = rho e^{i theta_j},  rho = (1/3)^{1/4} D,
-
-        theta_j = { pi/2-phi, pi/2+phi, 3pi/2 - phi, 3pi/2 + phi}, phi = 1/2 arctan(sqrt{13/3})
-
-    """
-
-    prefactor =  complex(1.,0.)*2.*N.pi*hvF**2/D_cutoff
-
-    phi = 0.5*N.arctan(N.sqrt(13./3.))
-
-    rho = (1./3.)**(0.25)
-
-    list_angles =  N.array([ N.pi/2-phi, N.pi/2+phi, 3*N.pi/2-phi, 3*N.pi/2+phi])
-    list_phases =  N.exp(1j*list_angles) 
-
-    list_x  =  rho*list_phases 
-
-    list_POLES  =  D_cutoff*list_x
-
-    list_RESIDUES = []
-
-    for i, x_i in enumerate(list_x):
-
-        numerator = -x_i**5
-
-        denominator = complex(1.,0.)
-
-        for j, x_j in enumerate(list_x):
-            if j != i:
-                denominator = denominator*(x_i-x_j)
-
-        residue = prefactor*numerator/denominator
-
-        list_RESIDUES.append(residue)
-
-    list_RESIDUES = N.array(list_RESIDUES)
-
-    return list_POLES, list_RESIDUES 
-
-def get_gammaC(list_z):
-    """
-    THIS CODE IS OBSOLETE.
-
-    The "real" kernel function is given by
-
-        gamma(z) =  2 pi (hvF)^2/D ( D/z 1/ln[1-D^2/z^2])
-
-    This function is wildly non-analytic near zero. 
-
-    gammaC = gamma - gammaA
-
-    The argument of the function should always be offset by mu!
-
-    The returned value is in eV x a0^2
-    """
-
-    prefactor =  complex(1.,0.)*2.*N.pi*hvF**2/D_cutoff
-
-    gammaA = get_gammaA(list_z)
-
-    x = complex(1.,0.)*list_z/D_cutoff
-    x2= x*x
-
-    denominator = x*N.log(complex(1.,0.)-complex(1.,0.)/x2)
-
-    gamma = prefactor/denominator 
-
-    gammaC = gamma - gammaA   
-
-    return gammaC
 
 def get_gamma(list_z):
     """
@@ -168,24 +48,34 @@ def get_gamma(list_z):
 
     return gamma
 
-def get_KR(list_energies,delta_width):
 
-    KR = complex(1.,0.)*N.real( get_gamma(list_energies+1j*delta_width)) 
+def get_KR(list_energies,kernel_Gamma_width):
+
+    KR = complex(1.,0.)*N.real( get_gamma(list_energies+1j*kernel_Gamma_width)) 
 
     return KR
+
+def get_KI(list_energies,kernel_Gamma_width):
+
+
+    limit =  complex(1.,0.)*2.*N.pi*hvF**2/D_cutoff*kernel_Gamma_width/D_cutoff
+
+    KI = -complex(1.,0.)*N.imag( get_gamma(list_energies+1j*kernel_Gamma_width)) - limit
+
+    return KI
 
 
 class ScatteringKernel:
     """
     This class will essentially produce the scattering kernel function L, which is given by
 
-    L(xi,nhw) = f(xi) KR(xi+mu-nhw) + i KramerKronig (KCI)(xi+mu-nhw)
+    L(xi,nhw) = f(xi) KR(xi+mu-nhw) + i KramerKronig (KI)(xi+mu-nhw)
 
     The Kramers-Kronig integral will be performed on a dense mesh, and a spline will be used
     to interpolate function to any desired value.
     """
 
-    def __init__(self,mu,beta, delta):
+    def __init__(self,mu,beta, kernel_Gamma_width):
         """
         Build the Kramers-Kronig integral
         """
@@ -197,8 +87,7 @@ class ScatteringKernel:
         self.beta  = beta
 
         # broadening, to avoid singularities in scattering kernel
-        self.delta = delta
-
+        self.kernel_Gamma_width = kernel_Gamma_width
 
     def build_scattering_kernel(self):
         """
@@ -219,10 +108,7 @@ class ScatteringKernel:
         KK = KramersKronig(list_u)
 
         # Compute the anti-symmetric kernel
-        g_plus    = get_gamma(list_u+self.mu+1j*self.delta)
-        g_minus   = get_gamma(list_u+self.mu-1j*self.delta)
-
-        KI = (g_minus-g_plus)/(2.*1j)
+        KI = get_KI(list_u+self.mu,self.kernel_Gamma_width)
 
         # Compute the Fermi occupation factor. Note that the chemical potential should NOT be
         # passed to this function 
@@ -246,7 +132,7 @@ class ScatteringKernel:
 
         # prepare the splne
         self.splmake_tuple = splmake(self.list_x, self.list_KK_fKI )
-        write_splmake(self.splmake_tuple,filename,self.mu,self.beta,self.delta)
+        write_splmake(self.splmake_tuple,filename,self.mu,self.beta,self.kernel_Gamma_width)
 
     def read_spline_parameters(self,filename):
         """
@@ -254,12 +140,12 @@ class ScatteringKernel:
         """
 
         # prepare the splne
-        self.splmake_tuple, file_mu, file_beta, file_delta = read_splmake(filename)
+        self.splmake_tuple, file_mu, file_beta, file_kernel_Gamma_width = read_splmake(filename)
 
         tol = 1e-8
         if N.abs(file_mu   - self.mu) > tol or \
             N.abs(file_beta - self.beta) > tol or \
-              N.abs(file_delta- self.delta) > tol:
+              N.abs(file_kernel_Gamma_width - self.kernel_Gamma_width) > tol:
             print 'ERROR: the splmake_tuple file contains parameters inconsistent with this calculation'
             print 'EXITING NOW'
             sys.exit()
@@ -274,7 +160,7 @@ class ScatteringKernel:
         f = function_fermi_occupation(list_xi,0.,self.beta)
 
         # Compute the symmetric kernel
-        KR = N.real(get_gamma(list_xi+self.mu+1j*self.delta))
+        KR = get_KR(list_xi,self.kernel_Gamma_width)
 
         LR = complex(1.,0.)*f*KR    
 
@@ -290,7 +176,8 @@ class ScatteringKernel:
         u  = list_xi[:,N.newaxis] - list_eta_hw[N.newaxis,:] + self.mu
         f  = fermi_dirac[:,N.newaxis]
 
-        KR = N.real( get_gamma(u+1j*self.delta) ) 
+
+        KR = get_KR(u,self.kernel_Gamma_width)
 
         LR = complex(1.,0.)*f*KR
 
