08e4ab2d12b64302a33ac69210d357fff7ca2fb6
Bruno Rousseau <rousseau.bruno@gmail.com>
2014-12-17 10:11:58 -0500

    Playing with paths.

--- modules/module_ForceConstants_Dubay.py
+++ modules/module_ForceConstants_Dubay.py
@@ -17,9 +17,10 @@ def getDubayForceConstants():
 
     hostname = socket.gethostname()
 
-    if hostname == 'ferron.pmc.umontreal.ca':
+    if 'ferron' in hostname:
         datafile = open('/Users/Shared/Bruno/work/projects/Graphene_Fano/python_work/HardCoreKernelFano/modules/force_constants.txt')
-
+    elif 'installation' in hostname:
+        datafile = open('/Users/Bruno/work/Projects/fano_project/HardCoreKernelFano_5.0/modules/force_constants.txt')
     else:
         datafile = open('/RQusagers/roussea4/python_depository/HCF_5.0/modules/force_constants.txt')
 
