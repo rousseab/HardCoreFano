90dc35d11b2e6179ecf7dee2a0b157b2c08a0e06
Bruno Rousseau <rousseau.bruno@gmail.com>
2015-02-16 11:12:48 -0500

    Small change to account for paths on local machine.

--- modules/module_ForceConstants_Dubay.py
+++ modules/module_ForceConstants_Dubay.py
@@ -19,11 +19,10 @@ def getDubayForceConstants():
 
     if 'ferron' in hostname:
         datafile = open('/Users/Shared/Bruno/work/projects/Graphene_Fano/python_work/HardCoreKernelFano/modules/force_constants.txt')
-    elif 'installation' in hostname:
-        datafile = open('/Users/Bruno/work/Projects/fano_project/HardCoreKernelFano_5.0/modules/force_constants.txt')
-    else:
+    elif 'briaree' in hostname:
         datafile = open('/RQusagers/roussea4/python_depository/HCF_5.0/modules/force_constants.txt')
-
+    else: 
+        datafile = open('/Users/Bruno/work/Projects/fano_project/HardCoreKernelFano_5.0/modules/force_constants.txt')
 
     DubayForceConstants_cartesian = {}
     DubayForceConstants_radial    = {}
