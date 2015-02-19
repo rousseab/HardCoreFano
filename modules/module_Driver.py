21b72edcf0926c6403908204709979b213bfd133
Bruno Rousseau <rousseau.bruno@gmail.com>
2015-02-19 16:22:52 -0500

    Modified to account for new code.

--- modules/module_Driver.py
+++ modules/module_Driver.py
@@ -68,10 +68,6 @@ reload(module_Integrators)
 from module_Integrators    import *
 
 
-import module_Compute_Loop_Function_Product
-reload(module_Compute_Loop_Function_Product)
-from module_Compute_Loop_Function_Product import *
-
 import module_Kramers_Kronig
 reload(module_Kramers_Kronig)
 from module_Kramers_Kronig import *
