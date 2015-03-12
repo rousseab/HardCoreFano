a81c6a0d74654280ccce1e025c83319f4495205b
Bruno Rousseau <rousseau.bruno@gmail.com>
2015-03-12 16:08:11 -0400

    Main script updated to account for changes.

--- modules/HardCoreFano.py
+++ modules/HardCoreFano.py
@@ -15,9 +15,9 @@ import socket
 hostname = socket.gethostname().split('.')[0]
 
 if 'mbpdeinllation' in hostname:
-    top_dir = '/Users/Bruno/work/Projects/fano_project/HardCoreKernelFano_6.0/'
+    top_dir = '/Users/Bruno/work/Projects/fano_project/HardCoreKernelFano_8.0/'
 else:
-    top_dir = '/RQusagers/roussea4/python_depository/HCF_6.0/'
+    top_dir = '/RQusagers/roussea4/python_depository/HCF_8.0/'
 
 
 sys.path.append(top_dir)
@@ -32,59 +32,53 @@ from module_Driver import *
 input_error   = False
 
 args = sys.argv[1:]
-if len(args) != 26:
+if len(args) != 23:
         input_error = True
 
 try:
         mu     = N.float(args[0])
         T      = N.float(args[1])
 
-        nmax_coarse_smooth   = N.int(args[2])
-        nmax_fine_smooth     = N.int(args[3])
-        nblock_smooth        = N.int(args[4])
-
-        nmax_coarse_singular = N.int(args[5])
-        nmax_fine_singular   = N.int(args[6])
-        nblock_singular      = N.int(args[7])
-
-        delta_width         = N.float(args[8])
-        kernel_Gamma_width  = N.float(args[9])
+        nmax_coarse = N.int(args[2])
+        nmax_fine   = N.int(args[3])
+        nblock      = N.int(args[4])
 
 
-        hw_ph  = N.float(args[10])
-        q_ph1  = N.float(args[11])
-        q_ph2  = N.float(args[12])
+        Green_Gamma_width   = N.float(args[5])
+        kernel_Gamma_width  = N.float(args[6])
 
-        re_E1x = N.float(args[13])
-        im_E1x = N.float(args[14])
-        re_E1y = N.float(args[15])
-        im_E1y = N.float(args[16])
-        re_E1z = N.float(args[17])
-        im_E1z = N.float(args[18])
 
-        re_E2x = N.float(args[19])
-        im_E2x = N.float(args[20])
-        re_E2y = N.float(args[21])
-        im_E2y = N.float(args[22])
-        re_E2z = N.float(args[23])
-        im_E2z = N.float(args[24])
-        filename = args[25]
+        hw_ph  = N.float(args[7])
+        q_ph1  = N.float(args[8])
+        q_ph2  = N.float(args[9])
+
+        re_E1x = N.float(args[10])
+        im_E1x = N.float(args[11])
+        re_E1y = N.float(args[12])
+        im_E1y = N.float(args[13])
+        re_E1z = N.float(args[14])
+        im_E1z = N.float(args[15])
+
+        re_E2x = N.float(args[16])
+        im_E2x = N.float(args[17])
+        re_E2y = N.float(args[18])
+        im_E2y = N.float(args[19])
+        re_E2z = N.float(args[20])
+        im_E2z = N.float(args[21])
+        filename = args[22]
 except:
         input_error   = True
 
 
 if input_error:
         print 'Something is wrong with input parameters!'
-        print 'HardCoreFano.py  <mu> <T> <nk_grid_coarse_smooth> <nk_grid_fine_smooth> <nblocks_smooth> <<nk_grid_coarse_singular> <nk_grid_fine_singular> <nblocks_singular>  <Gamma> <hw_ph> <q_ph> <E_ph> <output_filename>'
+        print 'HardCoreFano.py  <mu> <T> <nk_grid_coarse> <nk_grid_fine> <nblocks>  <Green_Gamma_width> <kernel_Gamma_width> <hw_ph> <q_ph> <E_ph> <output_filename>'
         print '                mu            :     chemical potential, in eV'
         print '                T             :     temperature, in Kelvin'
-        print ' SMOOTH         nk_grid_coarse:     parameter specifying how dense the coarse k-grid will be for the smooth grid'
-        print ' SMOOTH         nk_grid_fine  :     parameter specifying how dense the fine k-grid will be for the smooth grid'
-        print ' SMOOTH         nblocks       :     parameter specifying how many coarse blocks will be made fine for the smooth grid'
-        print ' SINGULAR       nk_grid_coarse:     parameter specifying how dense the coarse k-grid will be for the singular grid'
-        print ' SINGULAR       nk_grid_fine  :     parameter specifying how dense the fine k-grid will be for the singular grid'
-        print ' SINGULAR       nblocks       :     parameter specifying how many coarse blocks will be made fine for the singular grid'
-        print '                delta_width   :     width for delta-functions, in eV'
+        print '                nk_grid_coarse:     parameter specifying how dense the coarse k-grid will be '
+        print '                nk_grid_fine  :     parameter specifying how dense the fine k-grid will be '
+        print '                nblocks       :     parameter specifying how many coarse blocks will be made fine '
+        print '           Green_Gamma_width  :     width for Green functions, in eV'
         print '           kernel_Gamma_width :     Broadening used in the scattering kernel function, in eV'
         print '                hw_ph         :     phonon energy, in eV'
         print '                q_ph          :     phonon q vector; two real numbers'
@@ -106,19 +100,19 @@ E_ph = N.array([ re_E1x +1j*im_E1x , re_E1y +1j*im_E1y , re_E1z +1j*im_E1z ,
 #================================================================================
 # Generate data!
 #================================================================================
+
+# do not refine the grid around Gamma; there should be no resonance there
 include_Gamma = False
 
-grid_smooth = TesselationDoubleGrid(nmax_coarse_smooth, nmax_fine_smooth, nblock_smooth, include_Gamma,clip_grid=False,clip_energy=0.)
+grid = TesselationDoubleGrid(nmax_coarse, nmax_fine, nblock, include_Gamma, clip_grid=False, clip_energy=0.)
 
-clip_energy   = N.abs(mu)+0.5
-grid_singular = TesselationDoubleGrid(nmax_coarse_singular, nmax_fine_singular, nblock_singular, include_Gamma,clip_grid=True,clip_energy=clip_energy)
 
 #================================================================================
 #       Compute the Hq function 
 #================================================================================
 
-OkComputer = Compute_Loop_Function(mu, beta, q_ph, E_ph, hw_ph, grid_singular, grid_smooth, kernel_Gamma_width, delta_width)
+OkComputer = Compute_Loop_Function(mu, beta, q_ph, E_ph, hw_ph, grid, kernel_Gamma_width, Green_Gamma_width)
 
 OkComputer.Compute_R_and_I()
 
-write_to_file(OkComputer, nmax_coarse_smooth, nmax_fine_smooth, nblock_smooth, nmax_coarse_singular, nmax_fine_singular, nblock_singular, hw_ph, filename)
+write_to_file(OkComputer, nmax_coarse, nmax_fine, nblock, hw_ph, filename)
