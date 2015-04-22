70e3df701f3f0d4e8eeac9b806c375b8e23b55ee
Bruno Rousseau <rousseau.bruno@gmail.com>
2015-04-22 16:19:49 -0400

    Fermi occupation plot.

--- plot/plot_Fermi_occupation.py
+++ plot/plot_Fermi_occupation.py
@@ -7,8 +7,8 @@ import scipy.special  as  SS
 # import the usual modules
 import numpy as N
 import matplotlib as mpl
+import seaborn; seaborn.set(font_scale=2)
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 
 
 kB = 8.6173324e-5 # eV/K
@@ -19,7 +19,8 @@ hw = 0.250 # eV
 
 list_xi = N.arange(-1.,1.,0.001)
 
-list_hw = [-0.300, 0.010,0.200,0.300]
+list_hw = [0.010,0.100,0.200]
+list_c  = ['r','g','b']
 
 
 h = 10  # figure height
@@ -34,19 +35,21 @@ ms  = 10 # marker size
 mew = 2  # marker edge width
 lw  = 3
 
-for hw in list_hw:
+for hw,c in zip(list_hw,list_c):
     F2 = function_fermi_occupation(list_xi-hw,0.,beta)
     F1 = function_fermi_occupation(list_xi,0.,beta)
 
     dF = F2-F1
-    ax1.plot(list_xi,dF,'-',lw=lw,label='$\hbar\omega$ = %3.1f meV'%(1000*hw))
+    ax1.plot(list_xi,dF,c+'-',lw=1.5*lw,label='$\hbar\omega$ = %3.1f meV'%(1000*hw),alpha=0.5)
 
+    #dF_approx = -hw*d_Fermi_dxi(list_xi,0.,beta)+0.5*hw**2*d2_Fermi_dxi2(list_xi,0.,beta)
+    dF_approx = -hw*d_Fermi_dxi(list_xi,0.,beta)
 
+    ax1.plot(list_xi,dF_approx,c+'--',lw=lw,label='$\hbar\omega$ = %3.1f meV'%(1000*hw))
 
 for ax in list_ax:
     ax.set_xlabel('$\\xi$ (eV)')
     ax.legend(loc=0,fancybox=True,shadow=True)
-    ax.grid(True,linestyle='-',color='grey',alpha=0.5)
 
     ax.legend(loc=0,fancybox=True,shadow=True)
 
@@ -55,11 +58,5 @@ ax1.set_ylabel('$f(\\xi-\hbar\omega)-f(\\xi)$')
 
 fig1.suptitle('Difference in Fermi-Dirac occupation factors')
 # adjust the figure so that there is no wasted space
-fig1.subplots_adjust(   left    =   0.12,
-                        bottom  =   0.15,
-                        right   =   0.95,
-                        top     =   0.95,
-                        wspace  =   None, 
-                        hspace  =   None)
 
 plt.show()
