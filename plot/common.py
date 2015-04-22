06db076988c1c1dd2809a828d9179c74cbeabce2
Bruno Rousseau <rousseau.bruno@gmail.com>
2015-04-22 16:18:26 -0400

    update.

--- plot/common.py
+++ plot/common.py
@@ -54,9 +54,9 @@ import module_HardCoreKernel
 reload(module_HardCoreKernel)
 from module_HardCoreKernel import *
 
-import module_I
-reload(module_I)
-from module_I              import *
+import module_J
+reload(module_J)
+from module_J              import *
 
 import module_Tests
 reload(module_Tests)
@@ -67,8 +67,6 @@ import module_Integrators
 reload(module_Integrators)
 from module_Integrators    import *
 
-
-
 import module_NETCDF
 reload(module_NETCDF)
 from module_NETCDF         import *
@@ -77,4 +75,6 @@ import module_Kramers_Kronig
 reload(module_Kramers_Kronig)
 from module_Kramers_Kronig import *
 
-
+import module_Driver
+reload(module_Driver)
+from module_Driver         import *
